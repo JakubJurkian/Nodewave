e63b9598b6cca016c231cd47e6f4240310fa160b
Jakub <kuba.jur03@gmail.com>
2024-03-22 11:14:22 +0100

    added tailwindcss & postcss and added basic home route

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,19 @@
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
 import express from 'express';
 
+
+import homeRoutes from '../src/routes/home.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', join(__dirname, 'views'));
+
+app.use(homeRoutes);
 
 app.listen(3000);
